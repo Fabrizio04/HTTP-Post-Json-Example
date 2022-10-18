11e46cb4db422ad9573cf3c22a357b8c08ab1c0c
Fabrizio Amorelli <amorellif94@gmail.com>
2022-10-18 13:55:12 +0200

    Batch

--- CurlRequest.bat
+++ CurlRequest.bat
@@ -2,4 +2,4 @@
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-curl -X PUT -u username:password -k http://192.168.1.45:9200/ -H "Content-Type: application/json; charset=UTF-8" -d @input.json
+curl -X POST -u username:password -k http://192.168.1.45:9200/ -H "Content-Type: application/json; charset=UTF-8" -d @input.json
